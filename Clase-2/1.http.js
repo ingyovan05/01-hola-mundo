367e889830deb52d62616774b474bf42396a8d2b
Yovan Solano <ingyovansolano@gmail.com>
2023-09-19 13:54:02 -0500

    Mover carpetas

---
+++ Clase-2/1.http.js
@@ -0,0 +1,14 @@
+const http = require('node:http')
+
+const { puertodisponible } = require('./free-port.js')
+
+const server = http.createServer((req, res) => {
+  console.log('Request received')
+  res.end('Hola mundo')
+})
+
+puertodisponible(3001).then(port => {
+  server.listen(port, () => {
+    console.log(`Servidor listening on port http://localhost:${port}`)
+  })
+})
