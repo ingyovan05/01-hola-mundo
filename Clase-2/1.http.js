f46b5aa3f8a54806b9795fd9ddfde186d3481bfd
Yovan Solano <ingyovansolano@gmail.com>
2023-09-19 15:56:56 -0500

    xxx

--- Clase-2/1.http.js
+++ Clase-2/1.http.js
@@ -6,13 +6,13 @@ const processrequest = (req, res) => {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
   if (req.url === '/') {
     res.statusCode = 200
-      res.end('<h1>Bienvenido a mi página de inicio</h1>')
+    res.end('<h1>Bienvenido a mi página de inicio</h1>')
   } else if (req.url === '/contacto') {
     res.statusCode = 200
-      res.end('<h1>Contacto</h1>')
+    res.end('<h1>Contacto</h1>')
   } else {
     res.statusCode = 404
-      res.end('<h1> 404 </h1>')
+    res.end('<h1> 404 </h1>')
   }
 }
 
