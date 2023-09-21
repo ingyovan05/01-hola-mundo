904ab2a29a924a8771d1270790bb4f117b69031c
Yovan Solano <ingyovansolano@gmail.com>
2023-09-21 13:57:52 -0500

    cambios 21 sept

--- Clase-2/3.express.js
+++ Clase-2/3.express.js
@@ -5,25 +5,25 @@ const PORT = process.env.PORT ?? 1234
 
 app.disable('x-powered-by')
 
-app.use(express.json())
+// app.use(express.json())
 
-// app.use((req, res, next) => {
-//   if (req.method != 'POST') return next()
-//   if (req.headers['content-type'] != 'application/json') return next()
+app.use((req, res, next) => {
+  if (req.method != 'POST') return next()
+  if (req.headers['content-type'] != 'application/json') return next()
 
-//   let body = ''
-//   // escuchar el evento data
-//   req.on('data', chunk => {
-//     body += chunk.toString()
-//   })
+  let body = ''
+  // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
 
-//   req.on('end', () => {
-//     const data = JSON.parse(body)
-//     data.timestamp = Date.now()
-//     req.body = data
-//     next()
-//   })
-// })
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    req.body = data
+    next()
+  })
+})
 
 app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
